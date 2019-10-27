8d0ddccbfc5d4d96bae0bd3bb3f0d345b5305a94
buntarb <buntarb@gmail.com>
2019-10-27 16:16:27 +0200

    wip

---
+++ scripts/test.js
@@ -0,0 +1,24 @@
+const fs = require('fs');
+const path = require('path');
+const marked = require('marked');
+const { sanitize } = require('dompurify');
+
+// Set options
+// `highlight` example uses `highlight.js`
+marked.setOptions({
+  renderer: new marked.Renderer(),
+//   highlight: function(code) {
+//     return require('highlight.js').highlightAuto(code).value;
+//   },
+  pedantic: false,
+  gfm: true,
+  breaks: false,
+  sanitize: true,
+  sanitizer: sanitize,
+  smartLists: true,
+  smartypants: false,
+  xhtml: false
+});
+
+// Compile
+console.log(marked(fs.readFileSync(path.resolve(__dirname, '../readme.md'), 'utf8')));
