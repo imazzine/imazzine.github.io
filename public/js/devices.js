73e6216170ffa2308673e9543088e463ec25df16
buntarb <buntarb@gmail.com>
2019-11-27 20:41:39 +0200

    wip

--- public/js/devices.js
+++ public/js/devices.js
@@ -1,4 +1,6 @@
 window['@imazzine'].load('devices').then((graphObj) => {
+  graphObj.zoom = 1;
+  graphObj.zoomingEnabled = false;
   graphObj.container = document.getElementById('cytoblock');
   window['@imazzine'].cy = cytoscape(graphObj);
 });
